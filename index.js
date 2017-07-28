1ce60105e8aba533f44a633a88f1c4d3c117b949
David Keimes <david.keimes@hs-duesseldorf.de>
2017-07-28 19:08:06 +0200

    Increased Request size limit

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var port = process.env.PORT || 3000;
 
 // register body-parser
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({limit: '50mb'}, { extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
 
 app.use(function(req, res, next) {
     //res.header("Access-Control-Allow-Origin", "*");
